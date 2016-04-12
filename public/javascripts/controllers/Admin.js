2f74b9ca0bb2c65e8ec015fa27654ad891f1e637
Mickey <develop.agility@gmail.com>
2016-04-12 09:43:52 -0600

    a lil err message style

--- public/javascripts/controllers/Admin.js
+++ public/javascripts/controllers/Admin.js
@@ -1,12 +1,21 @@
 app.controller('AdminController', function ($scope, $http) {
-  var LOCAL_API = 'http://localhost:3000/users';
-  var REMOTE_API = 'https://my-testing-api.herokuapp.com/users';
-  var Path = '/admin/login';
+  var LOCAL_API = 'http://localhost:3000';
   
   $scope.title = 'Admin';
 
+  $scope.adminSignup = function(name, email, password){
+    $http.post(LOCAL_API +'/admin/signup', {name: name, email: email, password: password})
+      .then(function(data){
+        if(data.data.error){
+          $scope.error = data.data.error
+        } else {
+          $location.path( "/browse" );
+        }
+      })
+  };
+
   $scope.adminLogin = function(email, password){
-    $http.post(REMOTE_API + Path, {email: email, password: password})
+    $http.post(LOCAL_API + '/admin/login', {email: email, password: password})
       .then(function(data){
         console.log(data);
       })
