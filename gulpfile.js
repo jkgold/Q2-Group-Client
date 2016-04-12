f85b8683efe4ff6d432894fcbc6ab8004ea0e0d1
Mickey <develop.agility@gmail.com>
2016-04-11 22:53:22 -0600

    switched to gulp-connect dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp'),
+    connect = require('gulp-connect'),
     sass = require('gulp-sass');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'public/',
+    port: 8080,
+    livereload: true,
+    fallback: 'public/index.html'
+  });
+});
+
+
+gulp.task('html', function () {
+  gulp.src('./public/*.html')
+    .pipe(connect.reload());
+});
+
 
 gulp.task('sass', () =>{
   gulp.src('src/scss/main.scss')
@@ -10,7 +27,8 @@ gulp.task('sass', () =>{
 });
 
 gulp.task('watch', () =>{
+  gulp.watch('public/*.html', ['html']);
   gulp.watch('src/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['connect', 'html', 'sass', 'watch']);
--- public/javascripts/controllers/Browse.js
+++ public/javascripts/controllers/Browse.js
@@ -2,14 +2,13 @@ app.controller('BrowseController', function ($scope, $http) {
   var LOCAL_API = 'http://localhost:3000/products';
   var REMOTE_API = 'https://my-testing-api.herokuapp.com/products';
 
-  $scope.title = 'Browse';
-  
   loadProductData();
+  
+  $scope.title = 'Browse';
 
   function loadProductData(){
     $http.get(LOCAL_API)
       .then(function (data) {
-        console.log(data);
         $scope.products = data.data;
       });
   }
